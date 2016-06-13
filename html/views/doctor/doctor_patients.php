fdfebc288f4859671d65e84c82efb706650e6fd7
Vincent Chan <v.chan36@gmail.com>
2016-06-13 11:55:34 -0700

    add patients table to patients page

--- html/views/doctor/doctor_patients.php
+++ html/views/doctor/doctor_patients.php
@@ -10,8 +10,41 @@ if (!isset($_SESSION['myusername']) || $_SESSION['role'] != "doctor") {
     header("location:../../login.php");
 }
 
+$myEID = $_SESSION['mypassword'];
+$patientQuery = "SELECT * FROM Patient_Attendedby WHERE eid = '$myEID'";
+$patientResult = $conn->query($patientQuery);
+
+$data = array();
+
+while($row = $patientResult->fetch_assoc()) {
+    $data[] = $row;
+}
+
+$colNames = array_keys(reset($data));
 
 ?>
+
+<h3>My Patients</h3>
+<table border="1">
+    <tr>
+        <?php
+           // print the header
+           foreach($colNames as $colName) {
+              echo "<th> $colName </th>";
+           }
+        ?>
+    </tr>
+    <?php
+        // print the rows
+        foreach($data as $row) {
+            echo "<tr>";
+            foreach($colNames as $colName) {
+                echo "<td>".$row[$colName]."</td>";
+            }
+            echo "</tr>";
+        }
+    ?>
+</table>
 
 <?php
     echo "<br>";
