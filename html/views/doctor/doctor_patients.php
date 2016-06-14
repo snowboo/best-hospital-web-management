504eae6e2c7b178b66a66aadedb78bacb3a49420
Vincent Chan <v.chan36@gmail.com>
2016-06-14 13:02:17 -0700

    [wip] initial checkin for saving each patient from table

--- html/views/doctor/doctor_patients.php
+++ html/views/doctor/doctor_patients.php
@@ -12,12 +12,12 @@ if (!isset($_SESSION['myusername']) || $_SESSION['role'] != "doctor") {
 
 $myEID = $_SESSION['mypassword'];
 // $patientQuery = "SELECT * FROM Patient_Attendedby WHERE eid = '$myEID'";
-$patientQuery = "SELECT fname as 'First Name', lname as 'Last Name', sex as Sex, count(*) as '# Prescriptions'
+$patientQuery = "SELECT fname as 'First Name', lname as 'Last Name', pa.carecardnum as CardNo, count(*) as '# Prescriptions'
 FROM Patient_Attendedby pa, Prescribes p
 WHERE pa.eid = '$myEID' AND p.carecardnum = pa.carecardnum
 GROUP BY pa.carecardnum
 UNION
-SELECT fname, lname, pa1.sex, 0
+SELECT fname, lname, pa1.carecardnum, 0
 FROM Patient_Attendedby pa1, Prescribes p1
 WHERE pa1.eid = '$myEID' AND pa1.carecardnum NOT IN (SELECT p2.carecardnum FROM Prescribes p2);";
 $patientResult = $conn->query($patientQuery);
@@ -46,16 +46,23 @@ $colNames = array_keys(reset($data));
     </tr>
     <?php
         // print the rows
+        $index = 0;
         foreach($data as $row) {
             echo "<tr>";
             foreach($colNames as $colName) {
                 echo "<td>".ucfirst($row[$colName])."</td>";
+
             }
+            // save each patient into the session
+            $_SESSION[$index . "_" . 'carecardnum'] = $row['CardNo'];
             echo "<td>"."<a href='/views/doctor/prescriptions.php' class='btn btn-success'>Prescribe</a>" ."</td>";
             echo "<td>"."<a class='btn btn-warning'>New Record</a>" ."</td>";
             echo "</tr>";
+            $index++;
         }
-        echo $data[0]['fname'];
+        // echo drake and john's care card numbers
+        echo $_SESSION['0_carecardnum'];
+        echo $_SESSION['1_carecardnum'];
     ?>
 </table>
 
