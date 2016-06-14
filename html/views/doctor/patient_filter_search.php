75c24cf547ad4459b67d47ee712b24e8394de995
yves-chan <chanyves@gmail.com>
2016-06-13 17:31:35 -0700

    patient filter

--- html/views/doctor/patient_filter_search.php
+++ html/views/doctor/patient_filter_search.php
@@ -8,20 +8,18 @@ session_start();
 include ($_SERVER["DOCUMENT_ROOT"] . "/resources/config.php");
 include ($_SERVER["DOCUMENT_ROOT"] . "/resources/ChromePhp.php");
 
-
-//TODO AS FUCCCCK
 $tbl_name="Patient_Attendedby"; // Table name
-$fnamec = $_POST['fnamec'];
-$fname = $_POST['fname'];
-$lnamec = $_POST['lnamec'];
-$lname = $_POST['lname'];
-$carecardnum = $_POST['carecardnum'];
-$carecardnumc = $_POST['carecardnumc'];
-$agec = $_POST['agc'];
-$age = $_POST['age'];
-$sexc = $_POST['sexc'];
-$addressc = $_POST['addressc'];
-$operator = $_POST['operator'];
+$fnamec = $_POST["fnamec"];
+$fname = $_POST["fname"];
+$lnamec = $_POST["lnamec"];
+$lname = $_POST["lname"];
+$carecardnum = $_POST["carecardnum"];
+$carecardnumc = $_POST["carecardnumc"];
+$agec = $_POST["agc"];
+$age = $_POST["age"];
+$sexc = $_POST["sexc"];
+$addressc = $_POST["addressc"];
+$operator = $_POST["operator"];
 
 $selectionArray = array($fnamec, $lnamec, $carecardnumc, $agec, $sexc, $addressc);
 //Remove empty values from selection array
@@ -38,17 +36,17 @@ foreach ($selectionArray as $attribute) {
 $where="";
 //remove empty values from selection array
 if (isset($carecardnum) && !empty($carecardnum)) {
-  $where = "carecardnum = $carecardnum AND " . $where;
+  $where = "carecardnum = '$carecardnum' AND " . $where;
 }
 
 //remove empty values from selection array
 if (isset($fname) && !empty($fname)) {
-  $where = "fname = $fname AND " . $where;
+  $where = "fname = '$fname' AND " . $where;
 }
 
 //remove empty values from selection array
 if (isset($lname) && !empty($lname)) {
-  $where = "lname = $lname AND " . $where;
+  $where = "lname = '$lname' AND " . $where;
 }
 
 //remove empty values from selection array
@@ -69,18 +67,41 @@ $colNames = array_keys(reset($data));
 
 $count = $result->num_rows;
 
-
-
 if ($count >= 1) {
-    $_SESSION['carecardnum'] = $carecardnum;
-    header('location:medicalrecords.php');
+  
 } else {
-    $err_not_found = "No patient found with carecard # " . $carecardnum;
+    $err_not_found = "No patient found with selected attributes";
     echo '<script type="text/javascript">
             alert("'.$err_not_found.'");
-            window.location="medicalrecordsearch.php";
+            window.location="patient_filter.php";
           </script>';
 }
-
+ 
 ?>
 
+<h3>Found Patients</h3>
+<table border="1">
+    <tr>
+        <?php
+           // print the header
+           foreach($colNames as $colName) {
+              echo "<th> $colName </th>";
+           }
+        ?>
+    </tr>
+    <?php
+        // print the rows
+        foreach($data as $row) {
+            echo "<tr>";
+            foreach($colNames as $colName) {
+                echo "<td>".$row[$colName]."</td>";
+            }
+            echo "</tr>";
+        }
+    ?>
+</table>
+
+<?php
+    echo "<br>";
+    require_once("../../resources/templates/footer.php");
+?>
