bf215310892a337a399e848b10b35a9007ba9a54
Vincent Chan <v.chan36@gmail.com>
2016-06-13 18:36:14 -0700

    styled patient filter and login page

--- html/login.php
+++ html/login.php
@@ -1,30 +1,59 @@
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-    <tr>
-        <form name="form1" method="post" action="checklogin.php">
-            <td>
-                <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-                <tr>
-                    <td colspan="3"><strong>Member Login </strong></td>
-                </tr>
-                <tr>
-                    <td width="78">Email</td>
-                    <td width="6">:</td>
-                    <td width="294"><input name="myusername" type="text" id="myusername"></td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td>:</td>
-                    <td><input name="mypassword" type="password" id="mypassword"></td>
-                </tr>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td><input type="submit" name="Submit" value="Login">
-                    <button onclick="history.go(-1);">Cancel </button></td>
-                </tr>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
-                </table>
-            </td>
-        </form>
-    </tr>
-</table>
+<style>
+body {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #eee;
+}
+
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+.form-signin .checkbox {
+  font-weight: normal;
+}
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+</style>
+
+<div class="container">
+
+  <form class="form-signin" name="form1" method="post" action="checklogin.php">
+    <h2 class="form-signin-heading">Please sign in</h2>
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input type="email" id="myusername" name="myusername" class="form-control" placeholder="Email address" required autofocus>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="mypassword" name="mypassword" class="form-control" placeholder="Password" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    <button class="btn btn-warning btn-lg btn-block" onclick="history.go(-1);">Cancel </button></td>
+  </form>
+
+</div>
--- html/views/doctor/patient_filter.php
+++ html/views/doctor/patient_filter.php
@@ -11,26 +11,32 @@ if (!isset($_SESSION['myusername']) || $_SESSION['role'] != "doctor") {
 }
 ?>
 
-<form method="post" action="patient_filter_search.php">
-    <h3>Select by:</h3> <br>
-    First Name <input type="checkbox" name="fnamec" id="fnamec" value="fname"> <br/>
-    Last Name <input type="checkbox" name="lnamec" id="lnamec" value="lname"> <br/>
-    Carecard Number <input type="checkbox" name="carecardnumc" id="carecardnumc" value="carecardnum"> <br/>
-    Age <input type="checkbox" name="agec" id="agec" value="age"> <br/> 
-    Gender <input type="checkbox" name="sexc" id="sexc" value="sex"> <br/>
-    Address <input type="checkbox" name="addressc" id="addressc" value="address"> <br/>
+<form class="form-inline" method="post" action="patient_filter_search.php">
+    <h3>Select by:</h3>
+    <br>
+        <div class="form-group">
+            First Name <input type="checkbox" name="fnamec" id="fnamec" value="fname"> <br/>
+            Last Name <input type="checkbox" name="lnamec" id="lnamec" value="lname"> <br/>
+        </div>
+        <div class="form-group">
+            Carecard Number <input type="checkbox" name="carecardnumc" id="carecardnumc" value="carecardnum"> <br/>
+            Age <input type="checkbox" name="agec" id="agec" value="age"> <br/>
+        </div>
+        <div class="form-group">
+            Gender <input type="checkbox" name="sexc" id="sexc" value="sex"> <br/>
+            Address <input type="checkbox" name="addressc" id="addressc" value="address"> <br/>
+        </div>
     <br/>
     <h3>Search by:</h3> <br>
-    First name: 
-    <input type="text" name="fname" id="fname"> <br/>
-    Last name:
-    <input type="text" name="lname" id="lname"> <br/>
-    Carecard number:
-    <input type="text" name="carecardnum" id="carecardnum"> <br/>
+    <div class="form-group">
+        First name: <input type="text" name="fname" id="fname"> <br/>
+    </div>
+    Last name: <input type="text" name="lname" id="lname"> <br/>
+    Carecard number: <input type="text" name="carecardnum" id="carecardnum"> <br/>
     Patient Age:
     "<","=","<",">=", "<=", "<>" <input type="text" name="operator" id="operator" value="<">
     <input type="number" name="age" id="age">
-    <input type="submit" name="submit" value="submit">
+    <input class="btn btn-success" type="submit" name="submit" value="submit">
 </form>
 
 <?php
